c4f5a00e7187945df3e26f2bfd6846c46a4aaed2
LordNedStark <osisiogubenjamin1@gmail.com>
2023-05-17 17:19:59 +0100

    frontend sheets

---
+++ my_vscode_frontend_code/first_stage/frontend/quotes/quotes.js
@@ -0,0 +1,38 @@
+
+function quoting (){
+const response = fetch('https://api.kanye.rest/')
+  .then(response => {
+    return response.json()
+    .then(({ quote }) => {
+
+    const cls = document.getElementsByClassName("cls")
+    const children = cls[0].childNodes
+    children[0].textContent = quote;
+         
+      });
+ 
+  })
+ 
+}
+
+
+  let counter = 0
+  
+    function nextButton(){
+      counter++
+      // console.log(counter)
+         quoting()
+      
+  
+    }
+    function previousQuote(){
+      let p = document.getElementById("quote");
+      p.innerHTML = quoteList[(quoteList.length - 1) - 1];
+      // quoteList.pop();
+  }
+    function previousButton(){
+     
+    }
+    document.getElementById("next").onclick = nextButton
+    document.getElementById("previous").onclick = previousButton
+
