c67d401d641ed28f4e5ec5d2e8680d8c4d8e0f31
LordNedStark <osisiogubenjamin1@gmail.com>
2023-05-18 16:36:46 +0100

    made picture background

--- my_vscode_frontend_code/first_stage/frontend/quotes/quotes.js
+++ my_vscode_frontend_code/first_stage/frontend/quotes/quotes.js
@@ -1,38 +1,88 @@
 
-function quoting (){
-const response = fetch('https://api.kanye.rest/')
-  .then(response => {
-    return response.json()
-    .then(({ quote }) => {
+var onlineList = []
 
+
+let onlineListCounter = 1
+class Online{
+
+  static fetchOnline(){
+   for(let i = 0; i< 30; i++){
+    const response = fetch('https://api.kanye.rest/')
+    .then(response => {
+      return response.json()
+      .then(({ quote }) => {
+        onlineList.push(quote);
+        });
+    })
+   }
+   
+  }
+
+ static quoting (){
+  // Online.fetchOnline()
+  // Online.fetchOnline()
+  console.log(onlineList[onlineListCounter])
+  const cls = document.getElementsByClassName("cls")
+  const text = cls[0].childNodes[0]
+  text.textContent = onlineList[onlineListCounter]
+  onlineListCounter++
+ 
+  }
+
+}  
+Online.fetchOnline()
+console.log(onlineList.length);
+let userQuoteList = []
+let myListCounter = 0
+ 
+class MyQuotes{
+
+   static quoting(){
     const cls = document.getElementsByClassName("cls")
-    const children = cls[0].childNodes
-    children[0].textContent = quote;
-         
-      });
- 
-  })
- 
+    const text = cls[0].childNodes[0]
+
+    if(myListCounter < userQuoteList.length){
+    text.textContent = userQuoteList[myListCounter];
+    myListCounter++
+    }
+    else myListCounter = 0
+    
+  }
 }
 
-// new shoe
-  let counter = 0
+var quoteOption = 0
+
+  function changeQuoteOption(){
+    if (quoteOption === 0) quoteOption = 1
+    else if (quoteOption === 1) quoteOption = 0
+  }
   
-    function nextButton(){
-      counter++
-      // console.log(counter)
-         quoting()
-      
-  
+
+  function nextButton(){
+      switch(quoteOption){
+        case 0: Online.quoting()
+        break
+        case 1: MyQuotes.quoting() 
     }
-    function previousQuote(){
+  }
+    function previousButton(){
       let p = document.getElementById("quote");
       p.innerHTML = quoteList[(quoteList.length - 1) - 1];
       // quoteList.pop();
   }
-    function previousButton(){
-     
-    }
+
+  function enter(){
+  const inputValue = document.getElementById("input").value;
+      
+      userQuoteList.push(inputValue);
+      document.getElementById("input").value = "";
+      
+  }
+
     document.getElementById("next").onclick = nextButton
     document.getElementById("previous").onclick = previousButton
+    document.getElementById("choice").onclick = changeQuoteOption
+    document.getElementById("enter").onclick = enter
+
+
 
